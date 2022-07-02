0a77bccad85a1271ac36aa99de1f12fc54fb6f0c
ShaRifShykh <77236625+ShaRifShykh@users.noreply.github.com>
2022-07-03 03:44:39 +0500

    UI Done

--- lib/views/profile/profile_screen.dart
+++ lib/views/profile/profile_screen.dart
@@ -1,10 +1,25 @@
+import 'package:dirtydeeds/values/constant_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: ConstantColors.whiteColor,
+      appBar: AppBar(
+        backgroundColor: ConstantColors.whiteColor,
+        elevation: 0.0,
+        title: Text(
+          "Profile",
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
   }
 }
